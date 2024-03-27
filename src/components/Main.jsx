36b407ea0817a2b6c5d1eba80e2121ea3ad11490
giovanagomes7 <giovana.vieira@estudante.ifms.edu.br>
2024-03-27 09:14:58 -0400

    atividade 1326
    hgh

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,35 @@ import { useState } from "react";
 
 export default function Main(){
     const [nome, setNome] = useState("");
-
+    const [telefone, setTelefone] = useState();
+    
+    const [listaContatos, setListaContatos] = useState ([]);
+    
+    const registrar = (event) => {
+        event.preventDefault();
+        alert("Olá");
+        setListaContatos([...listaContatos,{
+            nomeSalvo: nome,
+            telefoneSalvo: telefone
+        }]);
+    };
+    
+    console.table(listaContatos);
 
     return(
         <main>
-        <input
-        type="text"
-        name=""
-        id=""
-        onChange={(event)=> setNome(event.target.value)}/>
-        {nome}
+            <form action="" onSubmit={registrar}>
+                <label htmlFor="nome">nome</label>
+                <input type="text" name="" id="nome" onChange={(event)=> setNome(event.target.value)}/> 
+               
+                <label htmlFor="telefone">telefone</label>   
+                <input type="text" name="" id="telefone" onChange={(event) => setTelefone (event.target.value)}/>
+                <button>Enviar</button>
+                
+            {nome}
+            {telefone}
+       </form>
+        
         </main>
     )
 }
